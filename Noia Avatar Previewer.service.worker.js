4cec6b773ab532b3f2d86c6cc9b6ff4cdb429f57
Swecle <swecleisnice@gmail.com>
2026-04-15 17:23:03 +0800

    cha

--- Noia Avatar Previewer.service.worker.js
+++ Noia Avatar Previewer.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776244725|1102446659';
+const CACHE_VERSION = '1776244880|1256918042';
 /** @type {string} */
 const CACHE_PREFIX = 'Noia Avatar Prev-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
